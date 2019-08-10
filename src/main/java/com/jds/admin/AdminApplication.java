d32fc1d563ece8b3fe681165549193fcbbc4e199
wangzhuo <wxwz98@163.com>
2019-08-10 16:07:38 +0800

    修改配置使其可以适用于cloud环境

--- src/main/java/com/jds/admin/AdminApplication.java
+++ src/main/java/com/jds/admin/AdminApplication.java
@@ -1,9 +1,12 @@
-package com.tlong.admin;
+package com.jds.admin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author wangzhuo
+ */
 @EnableAdminServer
 @SpringBootApplication
 public class AdminApplication {
---
+++ src/main/java/com/jds/admin/confifg/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.jds.admin.confifg;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author wangzhuo
+ */
+@EnableWebSecurity
+public class WebSecurityConfig implements WebMvcConfigurer {
+
+    @Bean
+    public UserDetailsService userDetailsService() throws Exception {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        manager.createUser(User.withUsername("admin").password(encoder.encode("admin123456")).roles("administrator").build());
+        return manager;
+    }
+}
